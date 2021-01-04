2c10cbf5bcf56a9c8549af31d64b4ccc31fc233f
c1946094 <ShahS58@cardiff.ac.uk>
2021-01-04 18:54:57 +0000

    created class for confirmation tokens

---
+++ src/main/java/ons/group8/domain/ConfirmationToken.java
@@ -0,0 +1,30 @@
+package ons.group8.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class ConfirmationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="token_id")
+    private long tokenId;
+
+    @Column(name="confirmation_token")
+    private String confirmationToken;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    public ConfirmationToken(User user) {
+        this.user = user;
+        createdDate = new Date();
+        confirmationToken = UUID.randomUUID().toString();
+    }
+}
